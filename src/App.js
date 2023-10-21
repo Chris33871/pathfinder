de8733380f4776ac3fc66c04b2247d24385e7337
christian <christian.harries15@gmail.com>
2023-10-21 10:08:49 +0100

    :construction: adding styling and descriptions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import PathfindingVisualizer from "./PathfindingVisualizer/PathfindingVisualizer";
 import Navbar from "./Components/Navbar";
+import Legend from "./Components/Legend";
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <Legend></Legend>
       <div className="App--body">
         <PathfindingVisualizer></PathfindingVisualizer>
       </div>
